95293516e91387ca4b32bbf24a09c12b4ef5fd0e
Alanis Boeing <alanis.boeing@C02R6E7EH3QD.local>
2023-04-04 17:36:29 -0300

    Inicio projeto

---
+++ teste.js
@@ -0,0 +1,14 @@
+pessoa = {
+    "nome": "Klaus",
+    "idade": 39,
+    "responsaveis": [
+        { "nome": "Margareth", "tipo": "MAE"},
+        { "nome" : "Edelmundo", "tipo" : "PAI"}
+    ]
+};
+
+pessoa.responsaveis.forEach((responsavel) =>{
+    console.log(responsavel);
+}); 
+
+
